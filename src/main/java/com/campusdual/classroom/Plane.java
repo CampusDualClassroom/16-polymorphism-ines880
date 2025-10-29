ac9513ccf107113630867ac89fc8d5a409e396ff
Inés <ines.ramos@campusdual.com>
2025-10-29 16:07:06 +0100

    solved exercise 16

--- src/main/java/com/campusdual/classroom/Plane.java
+++ src/main/java/com/campusdual/classroom/Plane.java
@@ -1,22 +1,43 @@
 package com.campusdual.classroom;
 
-public class Plane {
+public class Plane implements IMachine {
 
     private final String name;
 
     public Plane(String name) {
+
         this.name = name;
     }
 
     public void takeOff() {
+
         System.out.println("El avión despega");
     }
 
     public void land() {
+
         System.out.println("El avión aterriza");
     }
 
     public void fly() {
+
         System.out.println("El avión está volando");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("El avion " + name + " esta arrancando ");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("El avión " + name + " esta parando");
+
+    }
+
+    @Override
+    public void maintenance() {
+        System.out.println("El avion " + name + " esta en mantenimiento");
+
     }
 }
--- src/main/java/com/campusdual/classroom/Tractor.java
+++ src/main/java/com/campusdual/classroom/Tractor.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public class Tractor{
+public class Tractor implements IMachine{
 
     protected int horsePower = 0;
 
@@ -9,11 +9,30 @@ public class Tractor{
     }
 
     public void forward() {
+
         System.out.println("El tractor avanza");
     }
 
     public void backward() {
+
         System.out.println("El tractor retrocede");
     }
 
+    @Override
+    public void start() {
+        System.out.println("El tractor de " + horsePower + " CV esta arrancando");
+
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("El tractor de " + horsePower + " CV esta parado");
+
+    }
+
+    @Override
+    public void maintenance() {
+        System.out.println("El tractor de " + horsePower + "CV esta en mantenimiento");
+
+    }
 }
